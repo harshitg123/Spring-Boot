a5f4d02a81ffc307e26577201e9a92bfc104fcdb
harshitg123 <harshit.28.sps@gmail.com>
2024-01-21 18:22:16 +0530

    setter injection on primitive data types

--- springcore/src/main/java/com/springcore/App.java
+++ springcore/src/main/java/com/springcore/App.java
@@ -14,8 +14,12 @@ public class App
         // System.out.println( "Hello World!" );
 
         ApplicationContext context = new ClassPathXmlApplicationContext("classpath*:config.xml");
-        Student student = (Student)context.getBean("student1");
+        Student student1 =(Student)context.getBean("student1");
+        Student student2 =(Student) context.getBean("student2");
+        Student student3 =(Student) context.getBean("student3");
         
-        System.out.println(student);
+        System.out.println(student1);
+        System.out.println(student2);
+        System.out.println(student3);
     }
 }
