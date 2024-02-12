11a9d9e9f9648aa7559965f154387a658d7139b8
harshitg123 <harshit.28.sps@gmail.com>
2024-02-12 23:21:17 +0530

    used exp lang to call static method used static variables and create objects

--- springcore/src/main/java/com/springcore/SpEL/Expression.java
+++ springcore/src/main/java/com/springcore/SpEL/Expression.java
@@ -11,6 +11,18 @@ public class Expression {
 
     @Value("#{110-9}")
     private int subtract;
+
+    @Value("#{ T(java.lang.Math).sqrt(144) }")
+    private double squareRoot;
+
+    @Value("#{ T(java.lang.Math).PI }")
+    private double piValue;
+
+    @Value("#{ new java.lang.String('Harshit Gupta') }")
+    private String name;
+
+    @Value("#{ new java.lang.Integer(44) }")
+    private int rollNum;
     
     public int getAdd() {
         return add;
@@ -24,8 +36,35 @@ public class Expression {
     public void setSubtract(int subtract) {
         this.subtract = subtract;
     }
+    public double getSquareRoot() {
+        return squareRoot;
+    }
+    public void setSquareRoot(double squareRoot) {
+        this.squareRoot = squareRoot;
+    }
+    public double getPiValue() {
+        return piValue;
+    }
+    public void setPiValue(double piValue) {
+        this.piValue = piValue;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getRollNum() {
+        return rollNum;
+    }
+    public void setRollNum(int rollNum) {
+        this.rollNum = rollNum;
+    }
     @Override
     public String toString() {
-        return "Expression [add=" + add + ", subtract=" + subtract + "]";
+        return "Expression [add=" + add + ", subtract=" + subtract + ", squareRoot=" + squareRoot + ", piValue="
+                + piValue + ", name=" + name + ", rollNum=" + rollNum + "]";
     }
+    
+    
 }
