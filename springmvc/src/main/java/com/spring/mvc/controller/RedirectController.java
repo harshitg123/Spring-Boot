15b35bfbe7c8d7d48f87848205ffe7c1b131c811
harshitg123 <harshit.28.sps@gmail.com>
2024-03-31 15:31:43 +0530

    redirect using redirectView

--- springmvc/src/main/java/com/spring/mvc/controller/RedirectController.java
+++ springmvc/src/main/java/com/spring/mvc/controller/RedirectController.java
@@ -3,6 +3,8 @@ package com.spring.mvc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.view.RedirectView;
+
 
 /**
  * RedirectController
@@ -11,11 +13,21 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class RedirectController {
 
+    // Redirecct using redirect prefix
     @RequestMapping(path = "/first", method=RequestMethod.GET)
     public String first() {
         System.out.println("inside first handeler......");
         return "redirect:/second";
     }
+
+    // Redirect using redirectView
+    @RequestMapping(path = "/redirect", method=RequestMethod.GET)
+    public RedirectView redirect() {
+        RedirectView redirectView = new RedirectView();
+        redirectView.setUrl("second");
+        return redirectView;
+    }
+    
     
     @RequestMapping(path = "/second", method=RequestMethod.GET)
     public String requestMethodName() {
