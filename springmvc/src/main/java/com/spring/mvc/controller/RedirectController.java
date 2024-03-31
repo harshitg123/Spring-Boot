e53d8583432ca9abc50a7294b34fc56a5ac44089
harshitg123 <harshit.28.sps@gmail.com>
2024-03-31 14:52:25 +0530

    redirect using prefix

--- springmvc/src/main/java/com/spring/mvc/controller/RedirectController.java
+++ springmvc/src/main/java/com/spring/mvc/controller/RedirectController.java
@@ -1,9 +1,27 @@
 package com.spring.mvc.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
  * RedirectController
  */
+
+@Controller
 public class RedirectController {
 
+    @RequestMapping(path = "/first", method=RequestMethod.GET)
+    public String first() {
+        System.out.println("inside first handeler......");
+        return "redirect:/second";
+    }
+    
+    @RequestMapping(path = "/second", method=RequestMethod.GET)
+    public String requestMethodName() {
+        System.out.println("Inside second handeler.....");
+        return "contact";
+    }
+    
     
 }
