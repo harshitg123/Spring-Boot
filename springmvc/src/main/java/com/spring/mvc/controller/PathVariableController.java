5aadb841082bd26a8b716c29b52186c51375facc
harshitg123 <harshit.28.sps@gmail.com>
2024-04-01 22:06:58 +0530

    controller exception handling

--- springmvc/src/main/java/com/spring/mvc/controller/PathVariableController.java
+++ springmvc/src/main/java/com/spring/mvc/controller/PathVariableController.java
@@ -1,20 +1,40 @@
 package com.spring.mvc.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 
 @Controller
 public class PathVariableController {
     
-    @RequestMapping(path = "/pathVariable/{userId}", method=RequestMethod.GET)
-    public String pathVariable(@PathVariable("userId") int userId) {
+    @RequestMapping(path = "/pathVariable/{userId}/{userName}", method=RequestMethod.GET)
+    public String pathVariable(@PathVariable("userId") int userId, @PathVariable("userName") String userName) {
+
+        int st = Integer.parseInt(userName);
 
         System.out.println(userId);
         return "complexForm";
+    }
+
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(value = NumberFormatException.class)
+    public String handelNumberFormatException(Model model){
+        model.addAttribute("error", "Number Format Exception");
+        return "handelErrorPage";
+    }
+
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(value = Exception.class)
+    public String handelAllErrors(Model model){
+        model.addAttribute("error", "Exception occured");
+        return "handelErrorPage";
     }
     
 }
