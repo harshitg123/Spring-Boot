93dbc401f4c3616ef5de98beac61ac347d7c8742
harshitg123 <harshit.28.sps@gmail.com>
2024-04-07 19:20:10 +0530

    spring boot mvc - jsp

---
+++ springbootmvc/src/main/java/com/springboot/springbootmvc/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.springboot.springbootmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String homeController() {
+        System.out.println("Inside home controller....");
+        return "home";
+    }
+}
