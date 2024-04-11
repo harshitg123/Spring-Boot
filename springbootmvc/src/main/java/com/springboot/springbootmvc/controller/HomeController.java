9dfdeae96945b5306d0c7ac738dc94598c48ac00
harshitg123 <harshit.28.sps@gmail.com>
2024-04-11 13:39:33 +0530

    spring boot dev tools

--- springbootmvc/src/main/java/com/springboot/springbootmvc/controller/HomeController.java
+++ springbootmvc/src/main/java/com/springboot/springbootmvc/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.springboot.springbootmvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
@@ -10,5 +11,16 @@ public class HomeController {
     public String homeController() {
         System.out.println("Inside home controller....");
         return "home";
+    }
+
+    @GetMapping("/test")
+    @ResponseBody
+    public String TestController() {
+        System.out.println("Inside test controller.....");
+        int a = 34;
+        int b = 44;
+        int c = 24;
+        int d = 33;
+        return "Sum of integers are " + (a + b + c + d);
     }
 }
