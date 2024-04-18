5a3169fee5b7cbed0bf8b05a00805843d78b1dcd
harshitg123 <harshit.28.sps@gmail.com>
2024-04-18 19:17:56 +0530

    conditional statements

--- thymeleaf/src/main/java/com/thymleaf/thymeleaf/Controller/IteratorConroller.java
+++ thymeleaf/src/main/java/com/thymleaf/thymeleaf/Controller/IteratorConroller.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IteratorConroller {
@@ -15,6 +16,18 @@ public class IteratorConroller {
         List<String> friends = List.of("Harshit", "Ayush", "Kuljeet");
         model.addAttribute("friends", friends);
         return "iterator";
+    }
+
+    @GetMapping("/conditional")
+    public String conditionalStatementString(Model model) {
+
+        model.addAttribute("isActive", true);
+        model.addAttribute("gender", "F");
+
+        List<String> friends = List.of("harshit", "Ayush");
+        model.addAttribute("friends", friends);
+
+        return "conditional";
     }
 
 }
