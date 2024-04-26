60c70c3f6f244500095dfaa9f20ef041d87867ed
harshitg123 <harshit.28.sps@gmail.com>
2024-04-26 17:31:58 +0530

    fixed form login alonf with the logout

--- smartcontactmanager/src/main/java/com/smartcontactmanager/smartcontactmanager/Configurations/SpringSecurityConfigurations.java
+++ smartcontactmanager/src/main/java/com/smartcontactmanager/smartcontactmanager/Configurations/SpringSecurityConfigurations.java
@@ -1,22 +1,16 @@
 package com.smartcontactmanager.smartcontactmanager.Configurations;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
