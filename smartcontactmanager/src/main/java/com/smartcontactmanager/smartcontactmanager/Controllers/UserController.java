66deca4ff5a4ef1ef1bcf533f5ecf76eb12a72b7
harshitg123 <harshit.28.sps@gmail.com>
2024-04-27 22:14:14 +0530

    startiing the user dashboard

--- smartcontactmanager/src/main/java/com/smartcontactmanager/smartcontactmanager/Controllers/UserController.java
+++ smartcontactmanager/src/main/java/com/smartcontactmanager/smartcontactmanager/Controllers/UserController.java
@@ -1,12 +1,19 @@
 package com.smartcontactmanager.smartcontactmanager.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.smartcontactmanager.smartcontactmanager.Dao.UserRepository;
+
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping("/dashboard")
     public String userDashboard() {
