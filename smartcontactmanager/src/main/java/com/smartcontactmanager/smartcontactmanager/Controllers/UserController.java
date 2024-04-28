c916f67bbbaf22d7f6d4e12859293b4289a7a438
harshitg123 <harshit.28.sps@gmail.com>
2024-04-28 11:05:21 +0530

    usign principle interface of spring security

--- smartcontactmanager/src/main/java/com/smartcontactmanager/smartcontactmanager/Controllers/UserController.java
+++ smartcontactmanager/src/main/java/com/smartcontactmanager/smartcontactmanager/Controllers/UserController.java
@@ -1,10 +1,14 @@
 package com.smartcontactmanager.smartcontactmanager.Controllers;
 
+import java.security.Principal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.smartcontactmanager.smartcontactmanager.Dao.UserRepository;
+import com.smartcontactmanager.smartcontactmanager.Entites.User;
 
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -16,7 +20,16 @@ public class UserController {
     private UserRepository userRepository;
 
     @GetMapping("/dashboard")
-    public String userDashboard() {
+    public String userDashboard(Model model, Principal principal) {
+
+        String username = principal.getName();
+        System.out.println("Username " + username);
+
+        User user = userRepository.findByEmail(username);
+        System.out.println(user);
+
+        model.addAttribute("user", user);
+
         return "normal/user-dashboard";
     }
 
