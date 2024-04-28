d3572bfcc931e83cef0ae8792a218b89f95bab9a
harshitg123 <harshit.28.sps@gmail.com>
2024-04-28 23:30:51 +0530

    side bar and add contact form

--- smartcontactmanager/src/main/java/com/smartcontactmanager/smartcontactmanager/Controllers/UserController.java
+++ smartcontactmanager/src/main/java/com/smartcontactmanager/smartcontactmanager/Controllers/UserController.java
@@ -8,9 +8,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.smartcontactmanager.smartcontactmanager.Dao.UserRepository;
+import com.smartcontactmanager.smartcontactmanager.Entites.Contact;
 import com.smartcontactmanager.smartcontactmanager.Entites.User;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/user")
@@ -19,15 +22,24 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping("/dashboard")
-    public String userDashboard(Model model, Principal principal) {
-
+    @ModelAttribute
+    public void commonData(Model model, Principal principal) {
         String username = principal.getName();
         User user = userRepository.findByEmail(username);
-
         model.addAttribute("user", user);
+    }
 
+    @GetMapping("/dashboard")
+    public String userDashboard(Model model, Principal principal) {
+        model.addAttribute("title", "User dashboard");
         return "normal/user-dashboard";
+    }
+
+    @GetMapping("/add-contact")
+    public String addContactForm(Model model) {
+        model.addAttribute("title", "Add contact");
+        model.addAttribute("contact", new Contact());
+        return "normal/add_contact_form";
     }
 
 }
--- smartcontactmanager/src/main/resources/static/js/script.js
+++ smartcontactmanager/src/main/resources/static/js/script.js
@@ -3,10 +3,13 @@ const toggleButton = () => {
         
         $(".side-bar").css("display", "none");
         $(".content").css("margin-left", "0%");
+        $(".closeHam").css("visibility", "visible");
 
     } else {
         
         $(".side-bar").css("display", "block");
         $(".content").css("margin-left", "20%");
+        $(".closeHam").css("visibility", "hidden")
+
     }
 }
