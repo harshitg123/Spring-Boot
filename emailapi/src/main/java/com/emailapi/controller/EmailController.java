828f0930c97d2e826437dfa81cec9dc40c168eb9
harshitg123 <harshit.28.sps@gmail.com>
2024-05-04 19:10:56 +0530

    Email api to send on any email

--- emailapi/src/main/java/com/emailapi/controller/EmailController.java
+++ emailapi/src/main/java/com/emailapi/controller/EmailController.java
@@ -2,11 +2,12 @@ package com.emailapi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.emailapi.Model.Email;
 import com.emailapi.service.EmailService;
 
 @RestController
@@ -15,9 +16,9 @@ public class EmailController {
     @Autowired
     private EmailService emailService;
 
-    @GetMapping("/sendEmail")
-    public ResponseEntity<String> getMethodName() {
-        boolean status = emailService.sendEmail();
+    @PostMapping("/sendEmail")
+    public ResponseEntity<String> getMethodName(@RequestBody Email email) {
+        boolean status = emailService.sendEmail(email.getTo(), email.getSubject(), email.getMessage());
 
         if (status) {
             return ResponseEntity.status(HttpStatus.OK).body("Email sent successfully!");
--- emailapi/src/main/java/com/emailapi/service/EmailService.java
+++ emailapi/src/main/java/com/emailapi/service/EmailService.java
@@ -1,7 +1,11 @@
 package com.emailapi.service;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Properties;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
 import io.github.cdimascio.dotenv.Dotenv;
@@ -12,21 +16,26 @@ import jakarta.mail.PasswordAuthentication;
 import jakarta.mail.Session;
 import jakarta.mail.Transport;
 import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeBodyPart;
 import jakarta.mail.internet.MimeMessage;
+import jakarta.mail.internet.MimeMultipart;
 
 @Service
 public class EmailService {
 
-    public boolean sendEmail() {
+    public boolean sendEmail(String to, String subject, String message) {
 
         Dotenv dotenv = Dotenv.load();
 
         String from = dotenv.get("EMAIL_FROM");
-        String to = dotenv.get("EMAIL_TO");
-        String subject = "Testing email - Email API";
-        String message = "This is the testing email from email api";
         String emailPwd = dotenv.get("EMAIL_PWD");
 
+        // return sendEmailText(from, to, subject, message, emailPwd);
+        return sendEmailAttachment(from, to, subject, message, emailPwd);
+
+    }
+
+    private boolean sendEmailText(String from, String to, String subject, String message, String emailPwd) {
         // Step 1: gmail host
         String gmailHost = "smtp.gmail.com";
 
@@ -75,6 +84,76 @@ public class EmailService {
             e.printStackTrace();
             return false;
         }
+    }
 
+    private boolean sendEmailAttachment(String from, String to, String subject, String message, String emailPwd) {
+        // Step 1: gmail host
+        String gmailHost = "smtp.gmail.com";
+
+        // Step 2: Get system properties
+        Properties systemProps = System.getProperties();
+
+        System.out.println(systemProps);
+
+        // Set important infro to properties obj
+        systemProps.put("mail.smtp.host", gmailHost);
+        systemProps.put("mail.smtp.port", 465);
+        systemProps.put("mail.smtp.ssl.enable", "true");
+        systemProps.put("mail.smtp.auth", "true");
+
+        // Step 3: get the session
+
+        Session session = Session.getInstance(systemProps, new Authenticator() {
+
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(from, emailPwd);
+            }
+
+        });
+
+        session.setDebug(true);
+
+        // Step 4: Compose email (text, multimedia)
+
+        MimeMessage m = new MimeMessage(session);
+
+        try {
+            m.setFrom(from);
+
+            m.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+            m.setSubject(subject);
+
+            // sending file
+            try {
+                File file = new ClassPathResource("/static/img/default.png").getFile();
+
+                MimeMultipart mimeMultipart = new MimeMultipart();
+
+                MimeBodyPart textMime = new MimeBodyPart();
+                MimeBodyPart fileMime = new MimeBodyPart();
+
+                textMime.setText(message);
+                fileMime.attachFile(file);
+
+                mimeMultipart.addBodyPart(fileMime);
+                mimeMultipart.addBodyPart(textMime);
+
+                m.setContent(mimeMultipart);
+
+                // Step 5: Send message using Transport.send()
+                Transport.send(m);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return false;
+            }
+
+            return true;
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 }
