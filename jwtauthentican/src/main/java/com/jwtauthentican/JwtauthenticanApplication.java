10a3fbdcc964c2ae8680c64c78fdbfe681258eb0
harshitg123 <harshit.28.sps@gmail.com>
2024-05-11 00:01:26 +0530

    starting jwt repo

---
+++ jwtauthentican/src/main/java/com/jwtauthentican/JwtauthenticanApplication.java
@@ -0,0 +1,13 @@
+package com.jwtauthentican;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtauthenticanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtauthenticanApplication.class, args);
+	}
+
+}
---
+++ jwtauthentican/src/test/java/com/jwtauthentican/JwtauthenticanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwtauthentican;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtauthenticanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
